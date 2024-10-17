5514bd06119cdb6d1f51307b67dcc4fc62761a10
Noe Ramirez <1210198@alumno.um.edu.mx>
2024-10-17 14:27:58 +0200

    Change: cambiando el registro de los post

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -27,7 +27,7 @@ class PostController extends Controller
             'imagen' => 'required'
         ]);
 
-        Post::create([
+        $request->user()->posts()->create([
             'titulo' => $request->titulo,
             'descripcion' => $request->descripcion,
             'imagen' => $request->imagen,
