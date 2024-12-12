bb46c3e5e62b31c25b18fa5fb1d200774e8b7183
Noe Ramirez <1210198@alumno.um.edu.mx>
2024-12-12 13:39:25 -0600

    Build: creando el componente para mostrar los post

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -76,22 +76,6 @@
     <section class="container mx-auto mt-10 p-5">
         <h1 class="font-black text-center text-4xl my-10">Publicaciones</h1>
 
-        @if ($posts->count())
-            <div class="grid md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6">
-                @foreach ($posts as $post)
-                    <div>
-                        <a href="{{ route('post.show', ['post' => $post, 'user' => $post->user]) }}">
-                            <img src="{{ asset('uploads') . '/' . $post->imagen }}" alt="Imagen del Post {{ $post->titulo }}">
-                        </a>
-                    </div>
-                @endforeach
-            </div>
-
-            <div class="my-10">
-                {{ $posts->links('pagination::tailwind') }}
-            </div>
-        @else
-            <p class="text-gray-600 uppercase text-center text-sm font-bold">Sin Publicaciones</p>
-        @endif
+        <x-listar-post :posts="$posts" />
     </section>
 @endsection
